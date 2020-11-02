0133e14dfad7261726fcc9e2f6e21fad9aed0a2b
Morphyish <thomas.sauques@gmail.com>
2020-11-02 15:59:25 +0100

    :sparkles: Added some components for testing purposes

---
+++ Button/Button.svelte
@@ -0,0 +1,24 @@
+<button on:click>
+    <slot />
+</button>
+
+<style>
+    button {
+        border: 1px solid #ccc;
+        background-color: #f4f4f4;
+        outline: none;
+    }
+
+    button:focus,
+    button:hover {
+        border-color: #666;
+    }
+
+    button:not(:disabled):active {
+        background-color: #ddd;
+    }
+
+    button:disabled {
+        color: #999;
+    }
+</style>
---
+++ index.js
@@ -0,0 +1,7 @@
+import Button from './Button'
+import Toggle from './Toggle'
+
+export {
+    Button,
+    Toggle,
+}
