6b2df360c751db0c242d62342a1e36387c5d2912
Morphyish <thomas.sauques@gmail.com>
2020-11-03 12:08:09 +0100

    :art: linting

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,9 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.js"
-  ],
-  "addons": [
-    "@storybook/addon-essentials"
-  ]
+    'stories': [
+        '../stories/**/*.stories.mdx',
+        '../stories/**/*.stories.js',
+    ],
+    'addons': [
+        '@storybook/addon-essentials',
+    ],
 }
