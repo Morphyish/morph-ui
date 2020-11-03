33235a2c9c782be4714e91bc5b524d11ea012455
Morphyish <thomas.sauques@gmail.com>
2020-11-03 12:08:30 +0100

    :lipstick: updated components

--- src/components/Button/Button.svelte
+++ src/components/Button/Button.svelte
@@ -1,5 +1,7 @@
 <button
         {...$$props}
+        class:primary
+        class:secondary
         use:useActions={use}
         use:forwardEvents
 >
@@ -10,17 +12,47 @@
     button {
         text-transform: uppercase;
         border-radius: 3px;
+        background: transparent;
+        border: 0;
         cursor: pointer;
+        outline: 0;
+
+        height: 2.25rem;
+        min-width: 4rem;
+        padding: 0 1rem;
+        color: var(--default-text);
+        transition: box-shadow .2s cubic-bezier(.25,.8,.25,1);
+    }
+
+    button.secondary {
+        color: var(--secondary-text);
+    }
+
+    button.primary {
+        color: var(--primary-text);
     }
 
     button:focus,
     button:hover {
+        border: 1px solid var(--default-border);
+    }
+
+    button.primary:focus,
+    button.primary:hover {
+        border-color: var(--primary-border);
+    }
+
+    button.secondary:focus,
+    button.secondary:hover {
+        border-color: var(--secondary-border);
     }
 
     button:not(:disabled):active {
+        box-shadow: var(--box-shadow-inset);
     }
 
     button:disabled {
+        color: var(--disabled-text);
     }
 </style>
 
@@ -28,7 +60,9 @@
     import { get_current_component } from 'svelte/internal'
     import { forwardEventsBuilder, useActions } from '../../utils'
 
-    export let use = [];
+    export let primary = false
+    export let secondary = false
+    export let use = []
 
     const forwardEvents = forwardEventsBuilder(get_current_component())
 </script>
--- src/components/Toggle/Toggle.svelte
+++ src/components/Toggle/Toggle.svelte
@@ -1,9 +1,11 @@
 <button
         {...$$props}
+        class:primary
+        class:secondary
+        class:checked
+        on:click={toggle}
         use:useActions={use}
         use:forwardEvents
-        on:click={toggle}
-        class:checked
 >
     <slot />
 </button>
@@ -12,14 +14,47 @@
     button {
         text-transform: uppercase;
         border-radius: 3px;
+        background: transparent;
+        border: 0;
         cursor: pointer;
+        outline: 0;
+
+        height: 2.25rem;
+        min-width: 4rem;
+        padding: 0 1rem;
+        color: var(--default-text);
+        transition: box-shadow .2s cubic-bezier(.25,.8,.25,1);
+    }
+
+    button.secondary {
+        color: var(--secondary-text);
+    }
+
+    button.primary {
+        color: var(--primary-text);
     }
 
     button:focus,
     button:hover {
+        border: 1px solid var(--default-border);
+    }
+
+    button.primary:focus,
+    button.primary:hover {
+        border-color: var(--primary-border);
+    }
+
+    button.secondary:focus,
+    button.secondary:hover {
+        border-color: var(--secondary-border);
     }
 
     .checked {
+        box-shadow: var(--box-shadow-inset);
+    }
+
+    button:disabled {
+        color: var(--disabled-text);
     }
 </style>
 
@@ -27,8 +62,10 @@
     import { get_current_component } from 'svelte/internal'
     import { forwardEventsBuilder, useActions } from '../../utils'
 
-    export let use = [];
+    export let primary = false
+    export let secondary = false
     export let checked = false
+    export let use = [];
 
     const forwardEvents = forwardEventsBuilder(get_current_component())
 
